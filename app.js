5ca04ca6df77028b485f81d0cb7993b670350978
kofi <sambonful99@hotmail.com>
2022-06-07 21:10:33 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,29 @@
+const reviews = [
+    {
+        name: "john",
+        location: "london, UK",
+        description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Earum deserunt quia beatae quasi sint omnis aliquid molestiae aliquam eum modi"
+    },
+    {
+        name: "john",
+        location: "london, UK",
+        description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Earum deserunt quia beatae quasi sint omnis aliquid molestiae aliquam eum modi"
+    },
+    {
+        name: "john",
+        location: "london, UK",
+        description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Earum deserunt quia beatae quasi sint omnis aliquid molestiae aliquam eum modi"
+    },
+    {
+        name: "john",
+        location: "london, UK",
+        description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Earum deserunt quia beatae quasi sint omnis aliquid molestiae aliquam eum modi"
+    },
+    {
+        name: "john",
+        location: "london, UK",
+        description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Earum deserunt quia beatae quasi sint omnis aliquid molestiae aliquam eum modi"
+    },
+    
+]
+
